1fa0d39e1430c8afdd62d4215592e19e27202f87
bhanu <bhanu@gmail.com>
2023-09-22 11:46:09 +0530

    springboot

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/home")
+	public String index() {
+		return "Spring Application is Running...";
+	}
+
+}
